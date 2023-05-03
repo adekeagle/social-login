3dff297d4f295820f2a35345ef37b3f6f5786060
adekdevcode <adekdevcode@gmail.com>
2023-05-03 19:06:46 +0200

    initial project

---
+++ src/main/java/pl/adcom/sociallogin/controller/HomeController.java
@@ -0,0 +1,18 @@
+package pl.adcom.sociallogin.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello, World!";
+    }
+
+    @GetMapping("/secured")
+    public String secured() {
+        return "I'm secured!";
+    }
+}
